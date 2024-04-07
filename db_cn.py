1448da397e21f313532789779c76d6a94a4df4e0
tengtengsnake <peitengtsai@gmail.com>
2024-04-07 14:22:44 +0800

    update

--- db_cn.py
+++ db_cn.py
@@ -1,9 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request 
+from flask import render_template
 from sqlalchemy import create_engine, text
 from apparent_temp import apparent_temp
 from water_price import get_water_price
 from mac_gen_to_uuid import uuid_gen
+from interval import calculate_interval
 
 db = SQLAlchemy() # It used to create an instance of the SQLAlchemy object. 
 
@@ -20,12 +22,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Create an Engine object to connect to the database
 engine = create_engine("mariadb+mariadbconnector://{user}:{pw}@localhost:3306/{db}".format(user=user, pw=pw, db=db))
- 
-@app.route('/read_data_from_user')
+
+# login interface
+@app.route('/login')
+def login_interface():
+    return render_template('index.html')
+
+@app.route('/read_data_from_db')
 def read_data_from_user():
  
     sql_cmd = """
-        SELECT * FROM User
+        SELECT * FROM Sensors
         """
     compiled_sql_cmd = text(sql_cmd)
 
@@ -34,7 +41,7 @@ def read_data_from_user():
     # print(query_data) # cursor result object
     data = query_data.fetchall() # get the data from object
     for row in data:
-        print(row,type(row))
+        print(row)
     return f""" User ID, Total Water, Total Money, Flow Control  {row}"""
  
 @app.route('/insert_data_from_sensors', methods = ["GET","POST"])
@@ -62,6 +69,97 @@ def insert_data_to_sensors():
         insert_data = conn.execute(compiled_sql_cmd)
         conn.commit()
     return "Insert Successful!"
+
+@app.route('/update_data_from_sensors', methods = ["GET","POST"])
+def update_data_to_sensors():
+    sensor_id = str(request.args.get('sensor_id'))
+    water_Flow_Speed = request.args.get('water_Flow_Speed')
+    airPressure = request.args.get('airPressure')
+    apparentTemp = request.args.get('apparentTemp')
+    realTemp = request.args.get('realTemp')
+    humidity = request.args.get('humidity')
+    waterLevel = request.args.get('waterLevel')
+    totalwater= request.args.get('totalwater')
+    
+    # 如果我們要修改資料表中的資料我們就會需要用到 UPDATE 
+    # with total water accumulate
+    # get the previous time first
+    sql_cmd = f"""
+    SELECT totalwater, time
+    FROM Sensors
+    WHERE sensor_id = "{sensor_id}"
+    """
+
+    compiled_sql_cmd = text(sql_cmd)
+
+    try:
+        with engine.connect() as conn:
+            # Execute the SELECT statement and fetch the first row
+            row = conn.execute(compiled_sql_cmd).fetchone()
+        # print(row,type(row)) # (33.0, datetime.datetime(2024, 4, 5, 18, 40, 12)) <class 'sqlalchemy.engine.row.Row'>
+        
+        if row:
+            desired_format = "%Y-%m-%d %H:%M:%S"
+            totalwater_value = str(row[0])
+            time_value = row[1].strftime(desired_format)
+            
+            print(f"Retrieved total water value: {totalwater_value}")
+            print(f"Retrieved time value: {time_value}")
+
+        else:
+            print(f"No data found for sensor ID: {sensor_id}")
+        
+    except Exception as e:
+        print(f"Error retrieving data: {e}")
+
+    # After got the previous time, it's time to calculate the time interval
+    interval_time = calculate_interval(time_value) # mins time_value = start_time
+    use_water = float(water_Flow_Speed) * float(interval_time)
+    totalwater = use_water + float(totalwater_value)
+    # And then update the totalwater 
+
+    sql_cmd = f"""
+    UPDATE Sensors
+    SET totalwater = {totalwater}, time = CURRENT_TIMESTAMP
+    WHERE sensor_id = "{sensor_id}"
+    """
+
+    compiled_sql_cmd = text(sql_cmd)
+
+    with engine.connect() as conn:
+        update_data = conn.execute(compiled_sql_cmd)
+        conn.commit()
+
+    return "Update Successful!"
+
+
+@app.route('/modify_data_from_sensors', methods = ["GET","POST"])
+def modify_data_to_sensors():
+    
+    sensor_id = str(request.args.get('sensor_id'))
+    water_Flow_Speed = request.args.get('water_Flow_Speed')
+    airPressure = request.args.get('airPressure')
+    apparentTemp = request.args.get('apparentTemp')
+    realTemp = request.args.get('realTemp')
+    humidity = request.args.get('humidity')
+    waterLevel = request.args.get('waterLevel')
+    totalwater= request.args.get('totalwater')
+    
+    # 如果我們要修改資料表中的資料我們就會需要用到 UPDATE
+    # without total water accumulate
+
+    sql_cmd = f"""
+    UPDATE Sensors
+    SET water_Flow_Speed = {water_Flow_Speed}, airPressure = {airPressure}, apparentTemp = {apparentTemp},realTemp = {realTemp}, humidity = {humidity}, waterLevel = {waterLevel}, totalwater = {totalwater}, time = CURRENT_TIMESTAMP
+    WHERE sensor_id = "{sensor_id}"
+    """
+
+    compiled_sql_cmd = text(sql_cmd)
+
+    with engine.connect() as conn:
+        insert_data = conn.execute(compiled_sql_cmd)
+        conn.commit()
+    return "Modify Successful!"
 
 if __name__ == "__main__":
     app.run(host= "0.0.0.0", port = 5000, debug = True)
