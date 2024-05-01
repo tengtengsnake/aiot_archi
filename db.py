e31f6d41080a66d687851889db2bfd5d1ef56c21
tengtengsnake <peitengtsai@gmail.com>
2024-05-01 18:11:03 +0800

    update

--- db.py
+++ db.py
@@ -285,6 +285,56 @@ def insert_data_from_sensors():
             insert_data_successful_message_json_string = json.dumps(insert_data_successful_message, indent=4)
             
             return insert_data_successful_message_json_string
-        
+
+# get all the data with specific user
+@app.route('/read_all_data_from_db', methods=['GET', 'POST'])
+def read_data_from_db():
+    username = request.args.get('username')
+
+    sql_cmd = f"""
+    SELECT * FROM Sensors JOIN ( SELECT username as u, sensor_id as s, max(time) as t FROM Sensors GROUP BY username, sensor_id ) AS M ON Sensors.username = M.u AND Sensors.sensor_id = M.s AND Sensors.time = M.t WHERE Sensors.username = "{username}" GROUP BY Sensors.username, Sensors.sensor_id
+    """
+
+    compiled_sql_cmd = text(sql_cmd)
+
+    data_list = []
+    
+    with engine.connect() as conn:
+        # Execute the SELECT statement and fetch all rows
+        rows = conn.execute(compiled_sql_cmd).fetchall()
+        for row in rows:  # single row for data
+            sensor_id = row[1]
+            water_Flow_Speed = row[2]
+            airPressure = row[3]
+            apparentTemp = row[4]
+            realTemp = row[5]
+            humidity = row[6]
+            waterLevel = row[7]
+            totalwater = row[8]
+            Ultraviolet_intensity = row[9]
+            LuminousIntensity = row[10]
+            Altitude = row[11]
+            desired_format = "%Y-%m-%d %H:%M:%S"
+            time_value = row[12].strftime(desired_format)
+
+            data_dict = {
+                "sensord_id": sensor_id,
+                "water_Flow_Speed": water_Flow_Speed,
+                "airPressure": airPressure,
+                "apparentTemp": apparentTemp,
+                "realTemp": realTemp,
+                "humidity": humidity,
+                "waterLevel": waterLevel,
+                "totalwater": totalwater,
+                "Ultraviolet_intensity": Ultraviolet_intensity,
+                "LuminousIntensity": LuminousIntensity,
+                "Altitude0": Altitude,
+                "time": time_value
+            }
+            data_list.append(data_dict.copy())
+        result = []
+        result.append(data_list)
+
+    return {'result':data_list}
 if __name__ == "__main__":
     app.run(host= "0.0.0.0", port = 5000, debug = True)
--- get_all_data.py
+++ get_all_data.py
@@ -1,7 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request
 from sqlalchemy import create_engine, text
-from datetime import datetime
 import json
 
 db = SQLAlchemy()  # It used to create an instance of the SQLAlchemy object.
@@ -23,62 +22,50 @@ def read_data_from_db():
     username = request.args.get('username')
 
     sql_cmd = f"""
-    SELECT * FROM Sensors JOIN ( SELECT username as u, sensor_id as s, max(time) as t FROM Sensors GROUP BY username, sensor_id ) AS M ON Sensors.username = M.u AND Sensors.sensor_id = M.s AND Sensors.time = M.t WHERE Sensors.username = "{username}" GROUP BY Sensors.username, Sensors.sensor_id 
+    SELECT * FROM Sensors JOIN ( SELECT username as u, sensor_id as s, max(time) as t FROM Sensors GROUP BY username, sensor_id ) AS M ON Sensors.username = M.u AND Sensors.sensor_id = M.s AND Sensors.time = M.t WHERE Sensors.username = "{username}" GROUP BY Sensors.username, Sensors.sensor_id
     """
 
     compiled_sql_cmd = text(sql_cmd)
 
     data_list = []
-    try:
-        with engine.connect() as conn:
-            # Execute the SELECT statement and fetch all rows
-            rows = conn.execute(compiled_sql_cmd).fetchall()
-            for row in rows:  # single row for data
-                sensor_id = row[1]
-                water_Flow_Speed = row[2]
-                airPressure = row[3]
-                apparentTemp = row[4]
-                realTemp = row[5]
-                humidity = row[6]
-                waterLevel = row[7]
-                totalwater = row[8]
-                Ultraviolet_intensity = row[9]
-                LuminousIntensity = row[10]
-                Altitude = row[11]
-                desired_format = "%Y-%m-%d %H:%M:%S"
-                time_value = row[12].strftime(desired_format)
+    
+    with engine.connect() as conn:
+        # Execute the SELECT statement and fetch all rows
+        rows = conn.execute(compiled_sql_cmd).fetchall()
+        for row in rows:  # single row for data
+            sensor_id = row[1]
+            water_Flow_Speed = row[2]
+            airPressure = row[3]
+            apparentTemp = row[4]
+            realTemp = row[5]
+            humidity = row[6]
+            waterLevel = row[7]
+            totalwater = row[8]
+            Ultraviolet_intensity = row[9]
+            LuminousIntensity = row[10]
+            Altitude = row[11]
+            desired_format = "%Y-%m-%d %H:%M:%S"
+            time_value = row[12].strftime(desired_format)
 
-                data_dict = {
-                    "sensord_id": sensor_id,
-                    "water_Flow_Speed": water_Flow_Speed,
-                    "airPressure": airPressure,
-                    "apparentTemp": apparentTemp,
-                    "realTemp": realTemp,
-                    "humidity": humidity,
-                    "waterLevel": waterLevel,
-                    "totalwater": totalwater,
-                    "Ultraviolet_intensity": Ultraviolet_intensity,
-                    "LuminousIntensity": LuminousIntensity,
-                    "Altitude0": Altitude,
-                    "time": time_value
-                }
-                data_list.append(data_dict.copy())
+            data_dict = {
+                "sensord_id": sensor_id,
+                "water_Flow_Speed": water_Flow_Speed,
+                "airPressure": airPressure,
+                "apparentTemp": apparentTemp,
+                "realTemp": realTemp,
+                "humidity": humidity,
+                "waterLevel": waterLevel,
+                "totalwater": totalwater,
+                "Ultraviolet_intensity": Ultraviolet_intensity,
+                "LuminousIntensity": LuminousIntensity,
+                "Altitude0": Altitude,
+                "time": time_value
+            }
+            data_list.append(data_dict.copy())
+        result = []
+        result.append(data_list)
 
-            result = []
-            result.append(data_list)
-            result = json.dumps({'result':result})
-
-        return result
-    except:
-        error_message = {
-            'status': 'error',
-            'code': 400,
-            'message': 'Bad Request: error occur'
-        }
-
-        error_message_json_string = json.dumps(error_message, indent=4)
-
-        return error_message_json_string
+    return {'result':data_list}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
